f1eeb4dc2a88289bc651a51589019301075b8482
ArthurRLZ <arthurpassos05@gmail.com>
2026-02-24 01:08:39 -0300

    Criação do controller e inputs, e alteração no renderer. Feitos por heitor

---
+++ build/CMakeFiles/pathfinder3d.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pathfinder3d.dir/src/app/main.cpp.o"
+  "CMakeFiles/pathfinder3d.dir/src/app/main.cpp.o.d"
+  "CMakeFiles/pathfinder3d.dir/src/model/BFS.cpp.o"
+  "CMakeFiles/pathfinder3d.dir/src/model/BFS.cpp.o.d"
+  "CMakeFiles/pathfinder3d.dir/src/model/Grid.cpp.o"
+  "CMakeFiles/pathfinder3d.dir/src/model/Grid.cpp.o.d"
+  "CMakeFiles/pathfinder3d.dir/src/view/Renderer.cpp.o"
+  "CMakeFiles/pathfinder3d.dir/src/view/Renderer.cpp.o.d"
+  "pathfinder3d"
+  "pathfinder3d.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pathfinder3d.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
