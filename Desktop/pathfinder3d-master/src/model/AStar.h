30a70ebbd176d19d522d74cd68fc424648222613
ArthurRLZ <arthurpassos05@gmail.com>
2026-09-05 14:00:09 -0300

    Rebasing

--- Desktop/pathfinder3d-master/src/model/AStar.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "model/Grid.h"
-#include "model/Cell.h"
-#include <vector>
-
-class AStar {
-public:
-    static bool run(Grid& grid, Cell start, Cell goal, std::vector<Cell>& path);
-};
